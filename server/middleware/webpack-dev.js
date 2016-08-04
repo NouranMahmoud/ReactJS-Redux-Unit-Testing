cd84ba52f2d78dcbd58965b49527a2700fab0239
Greg Kedge <greg.kedge@atsid.com>
2016-08-04 18:54:08 -0400

    Linted.

--- server/middleware/webpack-dev.js
+++ server/middleware/webpack-dev.js
@@ -12,16 +12,16 @@ export default function (compiler, publicPath) {
   const middleware = WebpackDevMiddleware(compiler, {
     publicPath,
     contentBase: paths.client(),
-    hot: true,
-    quiet: config.compiler_quiet,
-    noInfo: config.compiler_quiet,
-    lazy: false,
-    stats: config.compiler_stats
+    hot:         true,
+    quiet:       config.compiler_quiet,
+    noInfo:      config.compiler_quiet,
+    lazy:        false,
+    stats:       config.compiler_stats
   })
 
-  return async function koaWebpackDevMiddleware (ctx, next) {
+  return async function koaWebpackDevMiddleware(ctx, next) {
     let hasNext = await applyExpressMiddleware(middleware, ctx.req, {
-      end: (content) => (ctx.body = content),
+      end:       (content) => (ctx.body = content),
       setHeader: function () {
         ctx.set.apply(ctx, arguments)
       }
